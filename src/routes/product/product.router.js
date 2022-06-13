b8f2b09d85e7b64f9ef71ad06f3ff7c24dbf293c
gabriel-mireles <gabrielmirelesvaldez@gmail.com>
2022-06-13 13:05:01 -0400

    fix(product): upload image authorization
    Added authentication and authorization for uploadImage endpoint.

--- src/routes/product/product.router.js
+++ src/routes/product/product.router.js
@@ -5,7 +5,7 @@ const authorizationMiddleware = require("../../middlewares/auth/authorization.mi
 const authenticationMiddleware = require("../../middlewares/auth/authentication.middlware");
 const productRouter = express.Router();
 
-const authMiddleware = [
+const authMiddlewares = [
   authenticationMiddleware,
   authorizationMiddleware("admin"),
 ];
@@ -13,14 +13,14 @@ const authMiddleware = [
 productRouter
   .route("/")
   .get(productController.httpGetAllProducts)
-  .post(authMiddleware, productController.httpCreateProduct);
+  .post(authMiddlewares, productController.httpCreateProduct);
 
-productRouter.route("/uploadImage").post(productController.httpUploadImage);
+productRouter.route("/uploadImage").post(authMiddlewares, productController.httpUploadImage);
 
 productRouter
   .route("/:id")
   .get(productController.httpGetSingleProduct)
-  .patch(authMiddleware, productController.httpUpdateProduct)
-  .delete(authMiddleware, productController.httpDeleteProduct);
+  .patch(authMiddlewares, productController.httpUpdateProduct)
+  .delete(authMiddlewares, productController.httpDeleteProduct);
 
 module.exports = productRouter;
